b9d19a24641d5c817dd169d78e5550b9d11d5248
ian <me@iany.me>
2021-08-22 09:28:24 +0800

    fix: ignore git diff renames
    The `pre-commit` script uses following command to scan added and deleted
    files:
    
    ```
    git diff --cached --name-only --diff-filter=A -z $against -- "$ASSETS_DIR"
    git diff --cached --name-only --diff-filter=D -z $against -- "$ASSETS_DIR"
    ```
    
    Since a version, git detects renames by default and will not list
    renames in filter A and D.
    
    There's a command line parameter `--no-renames`, but I use the config
    option instead which is compatible with old git versions:
    
    ```
    git -c diff.renames=false diff --cached --name-only --diff-filter=A -z $against -- "$ASSETS_DIR"
    git -c diff.renames=false diff --cached --name-only --diff-filter=D -z $against -- "$ASSETS_DIR"
    ```
    
    Closes https://github.com/doitian/unity-git-hooks/issues/9

--- tests.bat
+++ tests.bat
@@ -5,6 +5,8 @@ PRE_COMMIT="$BATS_TEST_DIRNAME/scripts/pre-commit"
 setup() {
   git init repo >&2
   cd repo
+  git config user.email "bats@ci"
+  git config user.name "bats"
   mkdir Assets
 }
 
@@ -17,18 +19,31 @@ teardown() {
   touch Assets/assets
   git add Assets/assets
   run "$PRE_COMMIT"
+  echo "$output"
   [ "$status" -eq 1 ]
   [ "${lines[0]}" = "Error: Missing meta file." ]
 
   touch Assets/assets.meta
   git add Assets/assets.meta
-  run "$PRE_COMMIT"
-  [ "$status" -eq 0 ]
+  "$PRE_COMMIT"
 }
 
 @test "ignoring assets file which starts with dot" {
   touch Assets/.assets
   git add --force Assets/.assets
+  "$PRE_COMMIT"
+}
+
+@test "renaming directory" {
+  mkdir Assets/dir
+  touch Assets/dir/.gitkeep
+  touch Assets/dir.meta
+  git add --force --all
+  git commit -n -m 'add Assets/dir'
+  mv Assets/dir Assets/dir-new
+  git add --force --all
   run "$PRE_COMMIT"
-  [ "$status" -eq 0 ]
+  echo "$output"
+  [ "$status" -eq 1 ]
+  [ "${lines[0]}" = "Error: Redudant meta file." ]
 }
